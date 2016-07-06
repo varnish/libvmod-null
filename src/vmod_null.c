d507a7bc75ebe40d5bb850e8d341d46648d20648
Reza Naghibi <reza@naghibi.com>
2016-07-05 20:36:31 -0400

    Safety checks

--- src/vmod_null.c
+++ src/vmod_null.c
@@ -7,8 +7,14 @@
 VCL_VOID
 vmod_synth(const struct vrt_ctx *ctx, VCL_STRING name, VCL_INT len)
 {
+	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+
 	assert(ctx->method == VCL_MET_SYNTH ||
 	    ctx->method == VCL_MET_BACKEND_ERROR);
+
+	if (name == NULL || len <= 0) {
+		return;
+	}
 
 	CHECK_OBJ_NOTNULL((struct vsb*)ctx->specific, VSB_MAGIC);
 
